c261e4d9a234fc3f569a6ff16ba2fe665fc8e95d
naavy-jjain <navyajain576@gmail.com>
2025-03-12 18:44:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+const shortid = require("shortid");
+
+mongoose.connect("mongodb://localhost:27017/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/shortUrls", async (req, res) => {
+  const shortCode = shortid.generate();
+  await ShortUrl.create({ full: req.body.fullUrl, short: shortCode });
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
